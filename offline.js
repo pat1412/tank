1f60b435f71bbf9c9de37c4e5963a185ab2dd684
pat141243 <pattar2k@gmail.com>
2018-08-30 15:03:33 +0700

    tank

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1535615645,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/jeep-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/wall2-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/holp-sheet0.png",
+		"images/particles-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
